dc4fbc8294fba509b6884ae4a30995a1cef91d01
Joe Murphy <joe.murphy@gmail.com>
2013-01-07 20:42:06 -0700

    finished working draft of hoods.py -- works for the sake of getting the population of each hood

--- hoods.py
+++ hoods.py
@@ -2,7 +2,6 @@
 # Run a query against the crime CSV's
 import os
 import csv
-import csvkit
 import operator
 from collections import defaultdict
 from optparse import OptionParser
@@ -10,10 +9,6 @@ from optparse import OptionParser
 
 keys = ['INCIDENT_ID','OFFENSE_ID','OFFENSE_CODE','OFFENSE_CODE_EXTENSION','OFFENSE_TYPE_ID','OFFENSE_CATEGORY_ID','FIRST_OCCURRENCE_DATE','LAST_OCCURRENCE_DATE','REPORTED_DATE','INCIDENT_ADDRESS','GEO_X','GEO_Y','GEO_LON','GEO_LAT','DISTRICT_ID','PRECINCT_ID','NEIGHBORHOOD_ID']
 neighborhoods = ['wellshire', 'bear-valley', 'hilltop', 'cbd', 'university-hills', 'overland', 'speer', 'union-station', 'washington-virginia-vale', 'marston', 'north-capitol-hill', 'city-park', 'sloan-lake', 'five-points', 'sun-valley', 'westwood', 'cole', 'windsor', 'platt-park', 'jefferson-park', 'harvey-park', 'skyland', 'sunnyside', 'southmoor-park', 'ruby-hill', 'capitol-hill', 'barnum-west', 'harvey-park-south', 'dia', 'athmar-park', 'elyria-swansea', 'lowry-field', 'goldsmith', 'stapleton', 'chaffee-park', 'berkeley', 'washington-park', 'indian-creek', 'barnum', 'montbello', 'civic-center', 'hampden-south', 'globeville', 'city-park-west', 'clayton', 'northeast-park-hill', 'country-club', 'hale', 'mar-lee', 'lincoln-park', 'gateway-green-valley-ranch', 'west-highland', 'congress-park', 'regis', 'east-colfax', 'whittier', 'belcaro', 'hampden', 'fort-logan', 'college-view-south-platte', 'montclair', 'baker', 'kennedy', 'cherry-creek', 'cheesman-park', 'west-colfax', 'south-park-hill', 'cory-merrill', 'rosedale', 'valverde', 'university-park', 'auraria', 'north-park-hill', 'highland', 'villa-park', 'university', 'virginia-village', 'washington-park-west']
-
-def top_row(row):
-    # Take the top row of a CSV and turn that into a list we can use.
-    print row
 
 def get_uniques(field):
     # Write a list of unique values from a field
@@ -24,14 +19,6 @@ def get_uniques(field):
         values.append(record[field])
 
     print set(values)
-
-def get_neighborhood(location):
-    # If location's in the list return that location name,
-    # if not, return false
-    if location in neighborhoods:
-        return location
-
-    return False
 
 def open_csv(fn = '_input/census_neighborhood_demographics_2010.csv'):
     # Open the crime file for parsing.
@@ -48,6 +35,15 @@ def get_stats(field, value, csv):
     # Take a field column and a value column and return a dict
     print field, value
 
+def build_dict(csv):
+    # Loop through a two-column csv. The first column should be
+    # the key, the second the value. Return a dict of the csv.
+    census_list = []
+    for row in csv:
+        census_list.append((row[0], row[1]))
+
+    return dict(census_list)
+
 if __name__ == '__main__':
     # parse the arguments, pass 'em to the function
     parser = OptionParser()
@@ -60,4 +56,5 @@ if __name__ == '__main__':
          fn = '_input/census_neighborhood_demographics_2010.csv'
     crime_file = open_csv(fn)
     key = crime_file.next()
-    get_stats(key[1], key[2], crime_file)
+    print build_dict(crime_file)
+    #get_stats(key[1], key[2], crime_file)
