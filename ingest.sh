84a495977baae8784228367d0cddb521046f9686
Joe Murphy <joe.murphy@gmail.com>
2013-01-09 21:37:23 -0700

    added more months, years for breaking up the csvs

--- ingest.sh
+++ ingest.sh
@@ -7,6 +7,16 @@
 
 cd _input
 DATE=`date +'%F-%k'`
+THIS_MONTH=`date +'%m'`
+LAST_MONTH=`expr $THIS_MONTH - 1`
+LAST_LAST_MONTH=`expr $THIS_MONTH - 2`
+if [[ $LAST_MONTH -lt 1 ]]; then; $LAST_MONTH = `expr $LAST_MONTH + 12`; fi
+if [[ $LAST_LAST_MONTH -lt 1 ]]; then; $LAST_LAST_MONTH = `expr $LAST_LAST_MONTH + 12`; fi
+
+THIS_YEAR=`date +'%Y'`
+LAST_YEAR=`expr $THIS_YEAR - 1`
+LAST_LAST_YEAR=`expr $THIS_YEAR - 2`
+
 touch crime-current.csv
 wget -O crime-new.csv http://data.denvergov.org/download/gis/crime/csv/crime.csv
 diff crime-new.csv crime-current.csv > crime-newdiff.csv
