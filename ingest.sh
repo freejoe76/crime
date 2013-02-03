2c23c346fc972a0a3b78dd7f69484f288e581ac5
Joe Murphy <joe.murphy@gmail.com>
2013-02-02 22:09:23 -0700

    allow for testing of the ingest script, with or without csv download

--- ingest.sh
+++ ingest.sh
@@ -4,7 +4,29 @@
 #     1. Save a copy of those differences with a timestamp
 #     2. Save that copy of those differences to latestdiff.csv
 #     3. Update the current-year csv with the results, currentyear.csv
-# ***TODO: Incorporate a means of testing this script
+#
+# It takes two arguments:
+#     -t / --test, which skips all file-writing operations except initial download
+#     -n / --nodl, which skips the csv download, and assumes an existing new.csv file is in place.
+
+TEST=0
+NODOWNLOAD=0
+while [ "$1" != "" ]; do
+	case $1 in
+		-n | --nodl ) 
+			NODOWNLOAD=1
+			;;
+		-t | --test ) 
+			TEST=1
+			;;
+	esac
+	shift
+done
+
+if [[ $TEST -eq 1 ]]; then
+	echo "TEST: $TEST"
+	echo "NO DOWNLOAD? $NODOWNLOAD"
+fi
 
 cd _input
 DATE=`date +'%F-%k'`
@@ -14,18 +36,32 @@ THIS_YEAR=`date +'%Y'`
 LAST_YEAR=`expr $THIS_YEAR - 1`
 LAST_LAST_YEAR=`expr $THIS_YEAR - 2`
 
-THIS_MONTH=`date + '%m'`
+THIS_MONTH=`date +'%m'`
 THIS_MONTH_FULL=`date +'%Y-%m'`
 LAST_MONTH=`expr $THIS_MONTH - 1`
 LAST_MONTH_FULL=$THIS_YEAR-`expr $THIS_MONTH - 1`
-if [[ $LAST_MONTH -lt 1 ]]; then $LAST_MONTH = `expr $LAST_MONTH + 12`; fi
+if [[ $LAST_MONTH -lt 1 ]]; then $LAST_MONTH=`expr $LAST_MONTH + 12`; fi
 
 touch current.csv
-wget -O new.csv http://data.denvergov.org/download/gis/crime/csv/crime.csv
+
+# If we're testing, it's possible we won't want to download the csv.
+if [[ $NODOWNLOAD -eq 0 ]]; then wget -O new.csv http://data.denvergov.org/download/gis/crime/csv/crime.csv; fi
+
 diff new.csv current.csv > newdiff.csv
 DIFFCOUNT=`cat newdiff.csv | wc -l`
 
-if [[ $DIFFCOUNT -gt 0 ]]; then
+if [[ $TEST -eq 1 ]]; then
+	echo "# Number of diffs between new.csv and current.csv: $DIFFCOUNT"
+	echo "# TEST: These are the commands that would be run."
+	echo 'cp newdiff.csv latestdiff.csv'
+	echo 'mv newdiff.csv "archive-'$DATE'.csv"'
+	echo 'mv current.csv old.csv'
+	echo 'mv new.csv current.csv'
+	echo 'grep "'$THIS_YEAR'-" current.csv > currentyear.csv'
+	echo 'grep "'$LAST_YEAR'-" current.csv > lastyear.csv'
+	echo 'grep "'$THIS_MONTH'" current.csv > currentmonth.csv'
+
+elif [[ $DIFFCOUNT -gt 0 ]]; then
 	cp newdiff.csv latestdiff.csv
 	mv newdiff.csv "archive-$DATE.csv"
 	mv current.csv old.csv
@@ -35,6 +71,6 @@ if [[ $DIFFCOUNT -gt 0 ]]; then
 	grep "$THIS_MONTH" current.csv > currentmonth.csv
 fi
 
-echo "[$DATE] $DIFFCOUNT new entries" >> $LOGFILE
+if [[ $TEST -eq 0 ]]; then echo "[$DATE] $DIFFCOUNT new entries" >> $LOGFILE; fi
 
 
--- source.sh
+++ source.sh
@@ -2,3 +2,5 @@
 # Aliases of commonly used commands. A shortcut list.
 # Usage:
 # $ source source.sh
+alias gpa='git pull -a'
+
