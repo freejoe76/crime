d81cc24a6b6637b6dee977853b61a5ae28525ee7
Joe Murphy <joe.murphy@gmail.com>
2013-04-20 15:35:28 -0600

    more template flesh-out

--- site/public/application.py
+++ site/public/application.py
@@ -1,9 +1,9 @@
-from flask import Flask, url_for
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('home.html')
 
 @app.route('/neighborhood/')
 @app.route('/neighborhood/<neighborhood>/')
