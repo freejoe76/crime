49d8c08400ddf5df8a10aff31be69a1c8389f631
Joe Murphy <joe.murphy@gmail.com>
2013-04-22 21:28:00 -0600

    got the for-loop over locations going

--- update.bash
+++ update.bash
@@ -1,29 +1,27 @@
 #!/bin/bash
 # Get the new crime csv, if there is one, and if there is, then
 # update the database.
+# Sometimes we may not want to download the csv.
 # $ ./update.bash
 
 while [ "$1" != "" ]; do
     case $1 in
-        -l | --location ) shift
-            location=$1
+        -c | --csv ) shift
+            csv=$1
             ;;
     esac
     shift
 done
-divider='\n=============================================================\n=============================================================\n'
 
-python parse.py --action specific --crime murder --grep --location $location
-
-python parse.py --limit 20 --action recent --location $location
-
-python parse.py --limit 20 --crime violent --action recent --location $location
-
-python parse.py --action rankings --crime violent --location $location
-
-python parse.py --action rankings --crime violent --location $location --filename currentmonth
-
-python parse.py --action rankings --crime property --location $location
-
-python parse.py --action rankings --crime property --location $location --filename currentmonth
-
+LOCATIONS='capitol-hill'
+for location in $LOCATIONS; 
+do 
+  echo $location
+  python write.py --kill --action ticker --crime murder --grep --location $location
+  python write.py --kill --action recent --location $location
+  #python write.py --kill --crime violent --action recent --location $location
+  python write.py --action rankings --crime violent --location $location
+  #python write.py --action rankings --crime violent --location $location --filename currentmonth
+  python write.py --kill --action rankings --crime property --location $location
+  #python write.py --kill --action rankings --crime property --location $location --filename currentmonth
+done
