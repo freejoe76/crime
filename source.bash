fb08c2546e12416a93e9af442c58dd8509de744a
Joe Murphy <joe.murphy@gmail.com>
2013-05-14 22:55:25 -0600

    add some mongo aliases

--- source.bash
+++ source.bash
@@ -4,5 +4,6 @@
 # $ source source.sh
 alias gpa='git pull -a'
 
+alias fixmongo='sudo rm /var/lib/mongodb/mongod.lock; sudo -u mongodb mongod -f /etc/mongodb.conf --repair'
 alias setlocal='export DENVERCRIME_SETTINGS=/home/joe/crime/site/public/settings.cfg; echo "Python Environment: $DENVERCRIME_SETTINGS"'
 alias setprod='export DENVERCRIME_SETTINGS=/path/to/settings.cfg; echo "Python Environment: $DENVERCRIME_SETTINGS"'
