163f6fe8f2271001e85b6cfbdaac9ffcf4d61e76
Joe Murphy <joe.murphy@gmail.com>
2013-05-20 18:33:07 -0600

    added application.js

---
+++ site/public/static/js/application.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+    var aboutHash = window.location.hash;
+
+    $("#showabout").on( "click", function () {
+        if ($(window).scrollTop() >= 10) {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 500);
+        }
+        $(".aboutsection").css( "opacity","1" );
+        $("#blogtitle").css( "opacity","0" );
+        $("#descriptionheader").css( "opacity","0" );
+     });
+ 
+ 
+     $(".showblog").on( "click", function () {
+        $(".aboutsection").css( "opacity","0" );
+        $("#blogtitle").css( "opacity","1" );
+        $("#descriptionheader").css(  "opacity","1" );
+     });
+
+
+    $('.photo-slideshow').pxuPhotoset({
+                'rounded'   : 'false', // corners, all or false
+                 'exif'      : true
+            }, function() {
+                // callback
+    });
+
+
+     
+    if (aboutHash === "#about") {
+        $("#showabout").trigger('click');
+    }
+ 
+ 
+});
+    
+$(window).on('scroll', function() {
+    if ($(this).scrollTop() > 450)
+        return false;
+    $('.thelargeheader').css({
+        'opacity':  1-(($(this).scrollTop())/400),
+        'top': -($(this).scrollTop()/3)
+    });
+});
