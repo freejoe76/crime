c444c501cc807c0b06e4e56e0e27013331553fdc
Joe Murphy <joe.murphy@gmail.com>
2013-08-23 21:40:22 -0600

    added some more shortcuts

--- source.bash
+++ source.bash
@@ -8,3 +8,5 @@ alias fixmongo='sudo rm /var/lib/mongodb/mongod.lock; sudo -u mongodb mongod -f 
 alias setlocal='export DENVERCRIME_SETTINGS=/home/joe/crime/site/public/settings.cfg; echo "Python Environment: $DENVERCRIME_SETTINGS"'
 alias setprod='export DENVERCRIME_SETTINGS=/path/to/settings.cfg; echo "Python Environment: $DENVERCRIME_SETTINGS"'
 alias dbs="sudo mongod --config /etc/mongodb.conf"
+alias venv='. ~/crime/crime/bin/activate'
+alias sss="source source.bash"
