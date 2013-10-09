a96f5b7f89e14c9a47a99654b21dbfabeb860cba
Joe Murphy <joe.murphy@gmail.com>
2013-10-08 20:35:43 -0600

    Add shapely requirement

--- within.py
+++ within.py
@@ -11,6 +11,13 @@ from datetime import datetime, timedelta
 class Within:
     """ class Within allows us to ask the crime CSV's
         spatial questions.
+        This class when complete should be able to answer
+        questions such as:
+
+        1. Which crimes in this time period fell within the 
+        boundaries of this shapefile?
+
+        2. Which crimes fall with X miles of this point?
         """
 
     def __init__(self):
