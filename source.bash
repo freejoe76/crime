2fa999da9294f5bd0805fcd991086223f2115cd8
Joe Murphy <joe.murphy@gmail.com>
2014-06-01 21:27:39 -0600

    some of them are old

--- source.bash
+++ source.bash
@@ -4,10 +4,8 @@
 # $ source source.sh
 alias gpa='git pull -a'
 
-alias fixmongo='sudo rm /var/lib/mongodb/mongod.lock; sudo -u mongodb mongod -f /etc/mongodb.conf --repair'
 alias setlocal='export DENVERCRIME_SETTINGS=/home/joe/crime/site/public/settings.cfg; echo "Python Environment: $DENVERCRIME_SETTINGS"'
 alias setprod='export DENVERCRIME_SETTINGS=/path/to/settings.cfg; echo "Python Environment: $DENVERCRIME_SETTINGS"'
-alias dbs="sudo mongod --config /etc/mongodb.conf"
 alias venv='. ~/crime/CRIME/bin/activate'
 alias sss="source source.bash"
 
