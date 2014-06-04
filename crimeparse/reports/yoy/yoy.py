dd4506f06114158b046876eced5d995b260e8a3f
freejoe76 <joe.murphy@gmail.com>
2014-06-03 23:34:21 -0600

    Fix yoy iteration error, add some reports

--- crimeparse/reports/yoy/yoy.py
+++ crimeparse/reports/yoy/yoy.py
@@ -5,23 +5,27 @@ from report import Report
 from optparse import OptionParser
 
 report_items = [ 
-        #{ 'slug': 'violent', 'name': 'Violent', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'violent', 'grep': False  },
-        #{ 'slug': 'property', 'name': 'Property', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'property', 'grep': False },
-        #{ 'slug': 'drug-alcohol', 'name': 'Drug and Alcohol', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug-alcohol', 'grep': False},
+        { 'slug': 'violent', 'name': 'Violent', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'violent', 'grep': False  },
+]
+"""
         { 'slug': 'drug-sell', 'name': 'Drug: Selling', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug*sell', 'grep': True},
         { 'slug': 'drug-possess', 'name': 'Drug: Possession', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug*possess', 'grep': True},
-        #{ 'slug': 'drug-poss', 'name': 'Drug: Paraphernalia Possesion', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug-poss', 'grep': True},
+        { 'slug': 'car-theft', 'name': 'Car Thefts', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'theft-of-motor-vehicle', 'grep': False  },
+        { 'slug': 'bike-theft', 'name': 'Bike Thefts', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'theft-bicycle', 'grep': False  },
+        { 'slug': 'burglary-business', 'name': 'Burglary: Business', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'burglary-business', 'grep': True},
+        { 'slug': 'burglary-residence', 'name': 'Burglary: Residence', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'burglary-residence', 'grep': True},
+        { 'slug': 'property', 'name': 'Property', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'property', 'grep': False },
+        { 'slug': 'drug-alcohol', 'name': 'Drug and Alcohol', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug-alcohol', 'grep': False},
+        { 'slug': 'drug-poss', 'name': 'Drug: Paraphernalia Possesion', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug-poss', 'grep': True},
         { 'slug': 'drug-synth', 'name': 'Drug: synth', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug-synth', 'grep': True},
-        #{ 'slug': 'drug-opium', 'name': 'Drug: opium', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug-opium', 'grep': True},
-        """
+        { 'slug': 'drug-opium', 'name': 'Drug: opium', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug-opium', 'grep': True},
         { 'slug': 'drug', 'name': 'Drug', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug', 'grep': True},
         { 'slug': 'drug-marijuana', 'name': 'Drug: Marijuana', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug-mari', 'grep': True},
         { 'slug': 'drug-heroin', 'name': 'Drug: heroin', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug-heroin', 'grep': True},
         { 'slug': 'drug-cocaine', 'name': 'Drug: cocaine', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug-cocaine', 'grep': True},
         { 'slug': 'drug-pcs', 'name': 'Drug: pcs / other', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug-pcs', 'grep': True},
         { 'slug': 'drug-meth', 'name': 'Drug: Meth', 'report_type': 'specific', 'date_type': '', 'location': '', 'crime': 'drug-meth', 'grep': True},
-        """
-]
+"""
 
 if __name__ == '__main__':
     parser = OptionParser()
@@ -32,6 +36,7 @@ if __name__ == '__main__':
     (options, args) = parser.parse_args()
 
     for item in report_items:
+        print item
         item['date_type'] = options.date_type
         item['location'] = options.location
         for yearback in [0, 1, 2, 3]:
