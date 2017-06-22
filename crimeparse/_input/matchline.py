793237514282c3ed530cf4ba6024e514ba65bdb9
freejoe76 <jmurphy@denverpost.com>
2017-06-21 22:58:27 -0400

    Update matchline to handle new datestamp format

--- crimeparse/_input/matchline.py
+++ crimeparse/_input/matchline.py
@@ -3,17 +3,19 @@
 # output the entire line to stdout.
 # The seventh field is FIRST_OCCURRENCE_DATE
 from optparse import OptionParser
+import re
 
 if __name__ == '__main__':
     parser = OptionParser()
     (options, args) = parser.parse_args()
 
     # First arg is the filename, second arg is the string we're looking for.
+    search = re.compile(args[0])
     lines = open(args[1]).readlines()
     for line in lines:
         cells = line.split(',')
         # The seventh cell contains the date field we want to search.
-        if args[0] in cells[6]:
+        if search.search(cells[6]):
             if '\\' in line:
                 line.replace('\\', '')
             print line.rstrip()
