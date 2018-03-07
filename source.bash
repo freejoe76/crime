43d302a1b65a290442f82db8b2ade29d8499cadb
freejoe76 <jmurphy@denverpost.com>
2018-03-06 23:28:39 -0500

    Cleanup commit

--- source.bash
+++ source.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Aliases of commonly used commands. A shortcut list.
 # Usage:
-# $ source source.sh
+# $ source source.bash
 workon CRIME
 alias gpa='git pull -a'
 
 alias setlocal='export DENVERCRIME_SETTINGS=/home/joe/crime/site/public/settings.cfg; echo "Python Environment: $DENVERCRIME_SETTINGS"'
 alias setprod='export DENVERCRIME_SETTINGS=/path/to/settings.cfg; echo "Python Environment: $DENVERCRIME_SETTINGS"'
-alias weekly='./weeklyreport.bash; ./yoyreport.bash; ./monthoverreport.bash'
-alias venv='. ~/crime/CRIME/bin/activate'
+alias weekly='./dailyreport.bash --cityonly; ./weeklyreport.bash; ./yoyreport.bash; ./monthoverreport.bash'
+alias daily='./monthlyreport.bash; ./dailyreport.bash --nocity'
 
 alias ra='./parse.py --action rankings --crime property --location capitol-hill'
 alias re='./parse.py --action recent --crime violent --location capitol-hill'
